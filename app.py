7f085f5633977d3883cc53cc77c565acff873290
PaIsaHA <paisaha991@gmail.com>
2026-07-16 09:50:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import plotly.express as px
 import time
 import streamlit.components.v1 as components
 
+
 # --- 1. CONFIGURACIÓN Y ESTADO ---
 st.set_page_config(page_title="AAM SkillMatrix Pro", layout="wide")
 
